e39a5b18c25340eb7be12d40fe91e1e9596d5e61
tkabeee <tkabee@gmail.com>
2016-01-15 09:55:40 +0900

    constantsで定義した各値をstateに代入
    stateを取得するget関数を実装

--- gulp/src/stores/AppStore.js
+++ gulp/src/stores/AppStore.js
@@ -5,10 +5,24 @@ var EventEmitter = require("events").EventEmitter;
 var AppConstants = require("../constants/AppConstants");
 var assign = require("object-assign");
 
+var _state = {
+  query: AppConstants.INIT_QUERY,
+  lat: AppConstants.INIT_LAT,
+  lng: AppConstants.INIT_LNG,
+  within: AppConstants.INIT_WITHIN,
+  rpp: AppConstants.INIT_RPP,
+  zoom: AppConstants.INIT_ZOOM,
+};
+
 var AppStore = assign({}, EventEmitter.prototype, {
+
+  getAll: function() {
+    return _state;
+  },
 
 });
 
+// Register callback to handle all updates
 AppDispatcher.register(function(action) {
 
 });
