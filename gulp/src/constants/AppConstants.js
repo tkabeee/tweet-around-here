3bdb3d1190f759e3b35b27117d22027df2db2748
tkabeee <tkabee@gmail.com>
2016-01-20 09:49:40 +0900

    アクションで使用するアクションタイプのキー名を定義

--- gulp/src/constants/AppConstants.js
+++ gulp/src/constants/AppConstants.js
@@ -2,14 +2,9 @@ var keyMirror = require("keymirror");
 
 module.exports = keyMirror({
 
-  INIT_QUERY: null,
-  INIT_LAT: null,
-  INIT_LNG: null,
-  INIT_WITHIN: null,
-  INIT_RPP: null,
-  INIT_ZOOM: null,
-  INIT_DISTANCE: null,
-  DISTANCES: null,
-  UNITS: null
+  UPDATE_LAT_LNG: null,
+  UPDATE_DISTANCE: null,
+  UPDATE_ZOOM: null,
+  UPDATE_TWEETS: null,
 
 });
