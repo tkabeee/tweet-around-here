6355fb21b410eb640a2a5b518d63726673c0872e
tkabeee <tkabee@gmail.com>
2016-01-20 09:48:54 +0900

    アクションの実装

--- gulp/src/actions/AppActions.js
+++ gulp/src/actions/AppActions.js
@@ -5,6 +5,35 @@ var AppConstants = require("../constants/AppConstants");
 
 var AppActions = {
 
+  updateLatLng: function(lat, lng) {
+    AppDispatcher.dispatch({
+      actionType: AppConstants.UPDATE_LAT_LNG,
+      lat: lat,
+      lng: lng
+    });
+  },
+
+  updateDistance: function(distance) {
+    AppDispatcher.dispatch({
+      actionType: AppConstants.UPDATE_DISTANCE,
+      distance: distance
+    });
+  },
+
+  updateZoom: function(zoom) {
+    AppDispatcher.dispatch({
+      actionType: AppConstants.UPDATE_ZOOM,
+      zoom: zoom
+    });
+  },
+
+  updateTweets: function(data) {
+    AppDispatcher.dispatch({
+      actionType: AppConstants.UPDATE_TWEETS,
+      data: data
+    });
+  }
+
 };
 
 module.exports = AppActions;
