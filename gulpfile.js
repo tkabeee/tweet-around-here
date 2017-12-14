64e0212d5028dcf29458d026bbe15bf3897bb089
tkabeee <tkabee@gmail.com>
2017-12-14 23:33:16 +0900

    gulp default task

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ const SCSS_FILES = 'sass/*.scss'
 const SASS_DIR = 'sass'
 const CSS_DIR = 'css'
 
+gulp.task('default', ['dev-server'])
+
 gulp.task('build', () => {
   return webpackStream(webpackConfig, webpack)
     .pipe(gulp.dest('build'))
@@ -16,14 +18,9 @@ gulp.task('build', () => {
 
 gulp.task('dev-server', () => {
   const compiler = webpack(webpackConfig)
-
   new webpackDevServer(compiler, {
   }).listen(8080, 'localhost', (err) => {
-    if (err) throw new gutil.PluginError('dev-server', err)
-    gutil.log('[dev-server]', 'http://localhost:8080/index.html')
+    if (err) throw new gutil.PluginError('webpack-dev-server', err)
+    gutil.log('[webpack-dev-server]', 'http://localhost:8080/index.html')
   })
-})
-
-gulp.task('default', () => {
-  gulp.start('dev-server')
 })
