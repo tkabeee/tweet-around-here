bdd652d43e932629b48f589598ca3630d153c051
tkabeee <tkabee@gmail.com>
2017-12-14 22:19:18 +0900

    webpack + bable

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  // entry point
+  entry: [
+    // 'babel-polyfill',
+    './src/app.js',
+  ],
+  output: {
+    // dest dir
+    path: `${__dirname}/build`,
+    // dest file
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                // env を指定することで、ES2017 を ES5 に変換
+                // {modules: false} にしないと import 文が Babel によって CommonJS に変換され
+                // webpack の Tree Shaking 機能が使えない
+                ['env', {'modules': false}],
+                // React JSX
+                'react'
+              ]
+            }
+          }
+        ],
+        // node_modules は除外
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js"]
+  },
+  // ソースマップを有効
+  devtool: 'source-map'
+}
