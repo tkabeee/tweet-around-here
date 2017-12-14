cf7d782730ec480292c7d9623a0f63be1c8277ec
tkabeee <tkabee@gmail.com>
2017-12-14 23:47:39 +0900

    create app.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp')
 const gutil = require('gulp-util')
+const grename = require('gulp-rename');
+
 const webpack = require('webpack')
 const webpackConfig = require('./webpack.config')
 const webpackStream = require('webpack-stream')
@@ -16,8 +18,17 @@ gulp.task('build', () => {
     .pipe(gulp.dest('build'))
 })
 
+gulp.task('deploy', () => {
+  gulp.src('./build/bundle.js')
+    .pipe(grename('app.min.js'))
+    .pipe(gulp.dest('./js'))
+})
+
 gulp.task('dev-server', () => {
   const compiler = webpack(webpackConfig)
+  compiler.plugin('done', () => {
+    gulp.start('deploy')
+  })
   new webpackDevServer(compiler, {
   }).listen(8080, 'localhost', (err) => {
     if (err) throw new gutil.PluginError('webpack-dev-server', err)
