f2b2e73c48d616cd4888bed740741f9791edef15
tkabeee <tkabee@gmail.com>
2018-01-12 00:24:25 +0900

    やっぱり gulp-webserver使っとく

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
-const gutil = require('gulp-util')
-const grename = require('gulp-rename');
+const util = require('gulp-util')
+// const rename = require('gulp-rename');
+const webserver = require('gulp-webserver')
 
 const webpack = require('webpack')
 const webpackConfig = require('./webpack.config')
@@ -11,27 +12,33 @@ const SCSS_FILES = 'sass/*.scss'
 const SASS_DIR = 'sass'
 const CSS_DIR = 'css'
 
-gulp.task('default', ['dev-server'])
+gulp.task('default', ['build'], () => {
+ gulp.start('webserver');
+//  gulp.start('webpack-dev-server')
+ gulp.watch(['src/**/*.js'], ['build']);
+})
 
 gulp.task('build', () => {
   return webpackStream(webpackConfig, webpack)
     .pipe(gulp.dest('build'))
 })
 
-gulp.task('deploy', () => {
-  gulp.src('./build/bundle.js')
-    .pipe(grename('app.min.js'))
-    .pipe(gulp.dest('./js'))
+gulp.task("webserver", () => {
+  gulp.src('./')
+    .pipe(webserver({
+      host: 'localhost',
+      port: 8080,
+      livereload: true
+    }))
 })
 
-gulp.task('dev-server', () => {
-  const compiler = webpack(webpackConfig)
-  compiler.plugin('done', () => {
-    gulp.start('deploy')
-  })
-  new webpackDevServer(compiler, {
-  }).listen(8080, 'localhost', (err) => {
-    if (err) throw new gutil.PluginError('webpack-dev-server', err)
-    gutil.log('[webpack-dev-server]', 'http://localhost:8080/index.html')
-  })
-})
+// gulp.task('webpack-dev-server', () => {
+//   const compiler = webpack(webpackConfig)
+//   compiler.plugin('done', () => {
+//   })
+//   new webpackDevServer(compiler, {
+//   }).listen(8080, 'localhost', (err) => {
+//     if (err) throw new util.PluginError('webpack-dev-server', err)
+//     util.log('[webpack-dev-server]', 'http://localhost:8080/webpack-dev-server/index.html')
+//   })
+// })
