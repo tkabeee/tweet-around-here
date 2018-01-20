18af3be8ae2519bfbf79c4c9e6b525b5ddc74050
tkabeee <tkabee@gmail.com>
2018-01-20 13:43:50 +0900

    不要なコードを整理

--- src/components/MapSection.js
+++ src/components/MapSection.js
@@ -16,19 +16,15 @@ export default class MapSection extends React.Component {
   render() {
     return (
       <div id="mapSection" className="map-section">
-        <MapCanvas
-          // lat={states.lat}
-          // lng={states.lng}
-          // zoom={states.zoom}
-          // within={states.within}
-          // onSearchTweet={this._handleSearchTweet}
-        />
+        <MapCanvas />
         <div id="positionInfo">
           <p className="latlng">
             Lat:&nbsp;<span id="lat">{this.state.lat}</span>&nbsp;&nbsp;
             Lng:&nbsp;<span id="lng">{this.state.lng}</span>
           </p>
-          <p className="address"><span id="address">Address 取得中…</span></p>
+          <p className="address">
+            <span id="address">Address 取得中…</span>
+          </p>
         </div>
         <div className="link">develop by <a href="//github.com/tkabeee" target="_blank">tkabee</a>. ＠<a href="//twitter.com/TweetAroundHere" target="_blank">TweetAroundHere</a></div>
       </div>
