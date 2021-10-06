5a379e2749a9ae42fe957e8470aedf73a74ffdb4
tkabeee <tkabkondo@gmail.com>
2021-10-07 00:18:44 +0900

    webpack dev server をアップデートしたので設定も更新

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,11 @@ module.exports = {
     filename: "bundle.js"
   },
   devServer: {
-    contentBase: path.join(__dirname, ''),
-    port: 8080,
-    inline: true,
-    hot: true
+    static: {
+      directory: path.join(__dirname, ''),
+    },
+    compress: true,
+    port: 8080
   },
   module: {
     rules: [
